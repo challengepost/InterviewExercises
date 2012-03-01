9dd29939fb78b9315773fe65ce1b24c404b3afb3
Ross Kaffenberger <rosskaff@gmail.com>
2012-03-01 17:51:05 -0500

    Dan's interview

--- public/javascripts/silly_reverser.js
+++ public/javascripts/silly_reverser.js
@@ -1,1 +1,16 @@
 // SillyReverser
+var SillyReverser = function(){
+// This function takes in an array and reverses it in place.
+    this.reverse = function(array){
+// i -- loop iterator
+// temp -- disposable variable for swapping elements
+      var i, temp;
+
+      for(i=0; i<(array.length/2);i++){
+        temp = array[i];
+// (length - 1) due to zero based array
+        array[i] = array[(array.length-1)-i];
+        array[(array.length-1)-i] = temp;
+      }
+    }
+}
--- spec/javascripts/silly_reverser_spec.js
+++ spec/javascripts/silly_reverser_spec.js
@@ -1,10 +1,32 @@
 describe("SillyReverser", function() {
 
-  it("should reverse an array in place", function() {
+  it("should reverse an array in place, even number", function() {
+    var reverser = new SillyReverser;
+    var a = [1, 2, 3, 4, 5, 6];
+    reverser.reverse(a);
+    expect(a).toEqual([6, 5, 4, 3, 2, 1]);
+  });
+
+  it("should reverse an array in place, odd number", function() {
     var reverser = new SillyReverser;
     var a = [1, 2, 3, 4, 5];
     reverser.reverse(a);
     expect(a).toEqual([5, 4, 3, 2, 1]);
+  });
+
+
+  it("should reverse an array in place, empty", function() {
+    var reverser = new SillyReverser;
+    var a = [];
+    reverser.reverse(a);
+    expect(a).toEqual([]);
+  });
+
+  it("should reverse an array in place, single", function() {
+    var reverser = new SillyReverser;
+    var a = [1];
+    reverser.reverse(a);
+    expect(a).toEqual([1]);
   });
 
 });
